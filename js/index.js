d77583b8b3ff05209c487a5c46441bd281037d50
Michael <112738703+messex1@users.noreply.github.com>
2024-02-20 17:34:11 -0700

    JS added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
- 
+function updateInnerHTML (selector, string)
+{
+    document.querySelector(selector).innerHTML = string
+}
+
+updateInnerHTML('h1', "New Topic")
+
+
+function strong(string)
+{
+    return `<strong>${string}</strong>`
+}
+
+const template = strong('making things useful')
+console.log(template)
+
+const updateInnerHTML = function (selector, htmlString)
+{
+    document.querySelector(selector).insertAdjacentHTML('beforeend', htmlString)
+}
+
+updateInnerHTML('section',template)
