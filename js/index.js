41161ab386b8a5c1e2b3a138b953071c5e593747
Michael <112738703+messex1@users.noreply.github.com>
2024-02-23 20:00:45 -0700

    lab 2 complete

--- js/index.js
+++ js/index.js
@@ -1,22 +1,19 @@
-function updateInnerHTML (selector, string)
-{
+function updateInnerHTML (selector, string){
     document.querySelector(selector).innerHTML = string
 }
 
-updateInnerHTML('h1', "New Topic")
+updateInnerHTML('h1', "New Topic");
 
-
-function strong(string)
-{
+function strong(string){
     return `<strong>${string}</strong>`
 }
 
-const template = strong('making things useful')
-console.log(template)
+const template = strong('making things useful');
 
-const updateInnerHTML = function (selector, htmlString)
-{
-    document.querySelector(selector).insertAdjacentHTML('beforeend', htmlString)
+console.log(template);
+
+function appendHTML(selector, htmlString){
+    document.querySelector(selector).insertAdjacentHTML('beforeend', htmlString);
 }
 
-updateInnerHTML('section',template)
+appendHTML('section',template);
